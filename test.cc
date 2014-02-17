2337fcce291d034b16fc4ad2891ccb4d1c1e16fd
Atul Garg <atgarg@cise.ufl.edu>
2014-02-17 15:09:15 -0500

    First running version

--- test.cc
+++ test.cc
@@ -106,8 +106,6 @@ void test1 (int option, int runlen) {
 		tutil.write = true;
 	}
 	pthread_create (&thread2, NULL, consumer, (void *)&tutil);
-	cout<<"Test1 :::::::::::::::::::::::::::::::::::::::"<<endl;
-	sortorder.Print();
 	BigQ bq (input, output, sortorder, runlen);
 
 	pthread_join (thread1, NULL);
