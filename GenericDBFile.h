6f3edfc81dc5c172699b452c9bb5ccb0bed54274
ssanupam24 <ssanupam24@gmail.com>
2014-03-06 14:51:42 -0500

    Update GenericDBFile.h
    added enum mode and virtual keyword

--- GenericDBFile.h
+++ GenericDBFile.h
@@ -18,18 +18,19 @@ struct SortInfo
 class GenericDBFile {
 
 public:
-	GenericDBFile (); 
+	GenericDBFile ():mode(READ); 
+    enum Mode { READ, WRITE } mode; 
+	virtual int Create (char *fpath, fType file_type, void *startup) = 0;
+	virtual int Open (char *fpath) = 0;
+	virtual int Close () = 0;
 
-	int Create (char *fpath, fType file_type, void *startup) = 0;
-	int Open (char *fpath) = 0;
-	int Close () = 0;
+	virtual void Load (Schema &myschema, char *loadpath) = 0;
 
-	void Load (Schema &myschema, char *loadpath) = 0;
-
-	void MoveFirst () = 0;
-	void Add (Record &addme) = 0;
-	int GetNext (Record &fetchme) = 0;
-	int GetNext (Record &fetchme, CNF &cnf, Record &literal) = 0;
+	virtual void MoveFirst () = 0;
+	virtual void Add (Record &addme) = 0;
+	virtual int GetNext (Record &fetchme) = 0;
+	virtual int GetNext (Record &fetchme, CNF &cnf, Record &literal) = 0;
 
 };
 #endif
+
